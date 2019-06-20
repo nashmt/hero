0350af79bf7549d9a2d34c64ac83f401c3171214
Matt Nash <Matt.Nash@softwareaggov.com>
2019-06-20 15:41:44 -0400

    commit test

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Partner Portal';
+  title = 'Partner Portal by Software AG Government Solutions';
 }
